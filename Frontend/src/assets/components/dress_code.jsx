b5697ab03c6a9e1d431d93c40b6c203308c63d3d
prem prakash <jyotiprakashjkp@gmail.com>
2025-07-16 20:26:38 +0530

    Update dress_code.jsx

--- Frontend/src/assets/components/dress_code.jsx
+++ Frontend/src/assets/components/dress_code.jsx
@@ -1,3 +1,20 @@
 import React from "react";
 import "../css/adm.css";
 
+const Dress = () => {
+  return <div>
+
+        <div className="main">
+            <img src="../../../public/background.jpg" alt="" />
+            <button download>Download</button>
+        </div>
+        <div className="main">
+          <div className="map">
+            
+          </div>
+        </div>
+
+  </div>;
+};
+
+export default Dress;
